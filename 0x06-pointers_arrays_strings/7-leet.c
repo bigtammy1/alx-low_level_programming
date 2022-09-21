bd1de4a43d6c26c357f9b08844afd44145887d45
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-09-21 14:55:21 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,24 @@
-include "main.h"
+#include "main.h"
 
 /**
- * leet - encode string into 1337 leet
- * @s: string to manipulate
- * Return: string
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
 
 char *leet(char *s)
 {
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	for (j = 0; s[j] != '\0'; j++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
 	}
 	return (s);
