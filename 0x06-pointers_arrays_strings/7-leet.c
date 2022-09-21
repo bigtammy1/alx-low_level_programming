720af68e2a80a20d182312356285ce79e11b213c
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-09-21 14:47:03 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,6 @@ char *leet(char *s)
 {
 	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-
 	int i, j;
 
 	for (j = 0; s[j] != '\0'; j++)
@@ -21,6 +20,5 @@ char *leet(char *s)
 				s[j] = b[i];
 		}
 	}
-
 	return (s);
 }
