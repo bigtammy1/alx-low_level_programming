599adfffb4e9dee7a8273855bac6f23e7ac3bb46
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-10-13 14:12:52 -0700

    Variadic functions project

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,12 @@
-#ifndef VARIADIC_FUNC
-#define VARIADIC_FUNC
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-
-} printer_t;
 
 #endif
