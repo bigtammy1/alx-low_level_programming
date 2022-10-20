e940f901334714e358480302225195d5fa29b5d6
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-10-20 13:37:29 -0700

    Task done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * racefirst - to print before main.c
+ * Return: void
+ */
+
+void racefirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
