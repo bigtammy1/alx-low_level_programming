adc6106f015f3c063500a16adab18fcbf772ab08
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-10-25 14:13:30 -0700

    Delete Nodeint file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,46 +1,39 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * delete_nodeint_at_index - deletes a node at a given position.
- * @head: head of linked list
- * @index: index to delete
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_nodeint_at_index - delete a node at a position
+ * @head: pointer to the head node
+ * @index: index where node will be deleted
+ * Return: 1 if successful, -1 if failed
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current;
-	listint_t *tmp;
+	listint_t *temp, *dnode;
+	unsigned int i = 1;
 
-	if (head == NULL || (*head) == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	current = *head;
+	temp = *head;
 	if (index == 0)
 	{
-		if ((*head)->next)
-			(*head) = (*head)->next;
-		else
-			(*head) = NULL;
-		free(current);
-		return (1);
+		*head = (*head)->next;
+		free(temp);
 	}
-
-	while (index != 1)
-	{
-		if (current->next == NULL)
-			return (-1);
-
-		current = current->next;
-		--index;
-	}
-	tmp = current->next;
-	if (current->next->next)
-		current->next = current->next->next;
 	else
-		current->next = NULL;
-	free(tmp);
-
+	{
+		while (i < index)
+		{
+			temp = temp->next;
+			if (temp == NULL)
+				return (-1);
+			i++;
+		}
+		dnode = temp;
+		dnode = dnode->next;
+		temp->next = dnode->next;
+		free(dnode);
+	}
 	return (1);
 }
