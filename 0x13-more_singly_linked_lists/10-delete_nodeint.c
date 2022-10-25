8de42190e2178ff3b0e9fd01222de5fd75c53c11
Tamunosaki Erekosima <bigtammy1@gmail.com>
2022-10-25 14:21:13 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,83 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - delete a node at a position
- * @head: pointer to the head node
- * @index: index where node will be deleted
- * Return: 1 if successful, -1 if failed
+ * listint_len - Returns the number of nodes in a list
+ * @h: The head of the list
+ *
+ * Return: The number of nodes or 0
  */
+size_t listint_len(const listint_t *h)
+{
+	size_t numElem = 0;
 
+	while (h != NULL)
+	{
+		numElem++;
+		h = h->next;
+	}
+
+	return (numElem);
+}
+
+/**
+ * get_nodeint_at_index - Returns the number at a particular node index
+ * @head: Head of the list
+ * @index: Index No of node
+ *
+ * Return: Pointer to the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t size = listint_len(head);
+	listint_t *node = head;
+	size_t current = 0;
+
+	if (index > size || head == NULL)
+		return (NULL);
+
+	while (current < index)
+	{
+		node = node->next;
+		current++;
+	}
+	return (node);
+}
+
+/**
+ * delete_nodeint_at_index - Inserts a new node at index specified
+ * @head: Pointer to head
+ * @index: The index position to replace
+ *
+ * Return: 1 if successful, -1, if not
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *dnode;
-	unsigned int i = 1;
+	listint_t *node;
+	listint_t *before;
+	size_t size;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
-	temp = *head;
-	if (index == 0)
+	node = get_nodeint_at_index(*head, index);
+	before = *head;
+	size = listint_len(*head);
+
+	if (index > size)
+		return (-1);
+
+	if (before == node)
 	{
-		*head = (*head)->next;
-		free(temp);
+		*head = node->next;
+		free(node);
+		return (1);
 	}
-	else
+
+	while (before->next != node)
 	{
-		while (i < index)
-		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (-1);
-			i++;
-		}
-		dnode = temp;
-		dnode = dnode->next;
-		temp->next = dnode->next;
-		free(dnode);
+		before = before->next;
 	}
+	before->next = node->next;
+	free(node);
 	return (1);
 }
