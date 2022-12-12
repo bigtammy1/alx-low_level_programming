0f1586c9752b16023ebcc459de83c17e40053ecb
bigtammy1 <bigtammy1@gmail.com>
2022-12-12 20:51:06 +0000

    Task done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Nginika/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+wget -P /tmp https://github.com/bigtammy1/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
 export LD_PRELOAD=/tmp/iwin.so
